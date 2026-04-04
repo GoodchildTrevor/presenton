ba809bdd11adb0cf54e6f61d3d1a6252a2b282aa
Ruslan Kondrashuk <127975578+GoodchildTrevor@users.noreply.github.com>
2026-04-04 19:25:08 +0300

    chore: remove external vendor env vars from compose and set_env.py

--- servers/fastapi/utils/set_env.py
+++ servers/fastapi/utils/set_env.py
@@ -17,56 +17,12 @@ def set_ollama_url_env(value):
     os.environ["OLLAMA_URL"] = value
 
 
-def set_custom_llm_url_env(value):
-    os.environ["CUSTOM_LLM_URL"] = value
-
-
-def set_openai_api_key_env(value):
-    os.environ["OPENAI_API_KEY"] = value
-
-
-def set_openai_model_env(value):
-    os.environ["OPENAI_MODEL"] = value
-
-
-def set_google_api_key_env(value):
-    os.environ["GOOGLE_API_KEY"] = value
-
-
-def set_google_model_env(value):
-    os.environ["GOOGLE_MODEL"] = value
-
-
-def set_anthropic_api_key_env(value):
-    os.environ["ANTHROPIC_API_KEY"] = value
-
-
-def set_anthropic_model_env(value):
-    os.environ["ANTHROPIC_MODEL"] = value
-
-
-def set_custom_llm_api_key_env(value):
-    os.environ["CUSTOM_LLM_API_KEY"] = value
-
-
 def set_ollama_model_env(value):
     os.environ["OLLAMA_MODEL"] = value
 
 
-def set_custom_model_env(value):
-    os.environ["CUSTOM_MODEL"] = value
-
-
-def set_pexels_api_key_env(value):
-    os.environ["PEXELS_API_KEY"] = value
-
-
 def set_image_provider_env(value):
     os.environ["IMAGE_PROVIDER"] = value
-
-
-def set_pixabay_api_key_env(value):
-    os.environ["PIXABAY_API_KEY"] = value
 
 
 def set_disable_image_generation_env(value):
@@ -85,24 +41,12 @@ def set_extended_reasoning_env(value):
     os.environ["EXTENDED_REASONING"] = value
 
 
-def set_web_grounding_env(value):
-    os.environ["WEB_GROUNDING"] = value
-
-
 def set_comfyui_url_env(value):
     os.environ["COMFYUI_URL"] = value
 
 
 def set_comfyui_workflow_env(value):
     os.environ["COMFYUI_WORKFLOW"] = value
-
-
-def set_dall_e_3_quality_env(value):
-    os.environ["DALL_E_3_QUALITY"] = value
-
-
-def set_gpt_image_1_5_quality_env(value):
-    os.environ["GPT_IMAGE_1_5_QUALITY"] = value
 
 
 def set_flux_url_env(value):
